1374c461c2499d699754f466bf6378690b63f44a
Amirmahdi79 <a.alijani.p@gmail.com>
2022-11-24 14:05:10 +0330

    Add project files.

---
+++ GenericRepository.EFCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GenericRepository.EFCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
